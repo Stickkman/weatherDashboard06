39b83241102b0763132d710c981b4f9303c8b965
stickkman <stickkman@gmail.com>
2022-12-18 13:04:26 -0500

    fixed current weather code variable values

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,11 +42,44 @@ function convertNames(convertData){
                         var lon = data[0].lon; //long numerical form
                         console.log("Converted Lat: " + lat + "\nConverted Lon: " + lon); //test for conversion
                             tempLat = lat; tempLon = lon;
-                            weatherApi();
+                             
+                            // FIX - start forcast function weatherApi();
+                            presentWeather();
   
                 })
     
 }
+// function to get current weather conditions of city
+function presentWeather () {
+    console.log("Present Passed Lat: " + tempLat + "\nPresent Passed Lon: " + tempLon); //test if variables passed to next function
+    // custom url for current city data retrieval based on lat and lons
+    var latLonUrl ="https://api.openweathermap.org/data/2.5/weather?lat=" + tempLat + "&lon=" + tempLon + "&units=imperial&appid=" + geoKey;
+
+    fetch(latLonUrl)
+        .then(function (response) {
+            console.log(response.status);
+                if(response.status !== 200) { // check for status errors, if there are any alert
+                    alert("Error reaching Server" + "\n Status Code: " + response.status);
+
+                }
+                return response.json();
+        })
+            .then(function (data) {
+                var tempCity = data.name; // holds current city name
+                var tempTemp = data.main.temp; // holds current temp
+                var tempWind = data.wind.speed; // holds wind speed
+                var tempHumidity = data.main.humidity // holds humidty data
+                var tempIcon = data.weather[0].icon // holds icon data
+                console.log(data);
+                // tests for all data for current day --VV--
+        console.log("City: " + tempCity + " | Date: " + currentDate + " | Temp: "
+        + tempTemp + " | Wind: " + tempWind + " | Humidity: " + tempHumidity + " | Icon: " + tempIcon); 
+                    
+            })
+
+}
+
+
 
 function weatherApi(){
     console.log("Passed Lat: " + tempLat + "\nPassed Lon: " + tempLon); //test if variables passed to next function
