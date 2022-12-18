cf5ca3b964f87c2499ca56b816c4a41aa1b06074
stickkman <stickkman@gmail.com>
2022-12-17 19:25:00 -0500

    created function to convert city name to lat/lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var searchBtn = ('#searchBtn'); // variable for search button
 var citySearchedEl = document.querySelector('input').value
 
+var citySearched =''; // global variable for city input
+var geoKey ="c321f3556bedeb7d653f0b65cb45d13b"
+
 // function to handle searches after search button is pressed
 function searchHandler(event) {
     event.preventDefault(); // prevents default action of form submits
@@ -8,11 +11,36 @@ function searchHandler(event) {
     console.log(citySearched); // test
 
     
-    
+    convertNames(citySearched); // calls converter function
+
     document.querySelector('input').value =''; // clears search box
 }
 
+            
 
+
+// function to convert input city name to geo coordinates  
+
+function convertNames(convertData){
+    // api query url based upon city entered with key added    
+    var geoApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + convertData + "&limit=1&appid=" + geoKey;
+        
+        fetch(geoApi)
+            .then(function (response) {
+                console.log(response.status);
+                    if (response.status !== 200) {
+                        alert("Error reaching Server" + "\n Status Code: " + response.status);
+                    }
+                    return response.json();
+            })
+                .then(function (data) {  // gets lat and lon for city name that is passed in
+                    console.log(data);
+                        var lat = data[0].lat; //lat numerical form
+                        var lon = data[0].lon; //long numerical form
+                        console.log("Converted Lat: " + lat + "\nConverted Lon: " + lon);
+                })
+    // geo api key c321f3556bedeb7d653f0b65cb45d13b
+}
 
 
 
